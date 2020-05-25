fc71b245359859c7479ec5ee50ce641dd9e88c32
momsse <momssesystem@gmail.com>
2020-05-25 16:04:35 +0200

    :bulb: some examples

---
+++ examples/withLocalAuth.ts
@@ -0,0 +1,23 @@
+import 'reflect-metadata';
+import { Arg, Query, Resolver } from 'type-graphql';
+import logger from '@boilerz/logger';
+import mongoPlugin from '@boilerz/super-server-mongo';
+import authCorePlugin from '@boilerz/super-server-auth-core';
+import authLocalPlugin from '@boilerz/super-server-auth-local';
+
+import * as superServer from '../src';
+
+@Resolver()
+class GreetingResolver {
+  @Query(() => String)
+  public hello(@Arg('name') name: string): string {
+    return `Hello ${name}`;
+  }
+}
+
+superServer
+  .start({
+    resolvers: [GreetingResolver],
+    plugins: [mongoPlugin, authCorePlugin, authLocalPlugin],
+  })
+  .catch(err => logger.error({ err }, '[examples/withLocalAuth]'));
