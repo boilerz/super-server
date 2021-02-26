3f9cd8e4df3099d09359ac8a87d746e7f6008eb8
momsse <1344228+momsse@users.noreply.github.com>
2021-02-26 21:16:13 +0100

    :wrench: jest config update

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,1 @@
-module.exports = {
-  testEnvironment: 'node',
-  moduleFileExtensions: ['ts', 'js', 'json'],
-  transform: {
-    '^.+\\.ts$': 'ts-jest',
-  },
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
-  testPathIgnorePatterns: ['<rootDir>/dist', '<rootDir>/coverage'],
-};
+module.exports = require('@boilerz/jest-config');
